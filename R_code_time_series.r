ae6304f321f3fcb4f0d8a7526dc5247b74be2d07
nicole97ron <80778386+nicole97ron@users.noreply.github.com>
2021-04-05 18:37:44 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,7 @@
+# TIME SERIES ANALYSIS
+# Analizziamo l'incremento della temperatura in Groenlandia
+
+# install.packages("raster")
+library(raster)
+# percorso Windows per lavorare con la cartella greenland che si trova dentro la cartella lab
+setwd("C:/lab/greenland")
