292935cfac8527a0980979e0c18ec857f0400d3b
nicole97ron <80778386+nicole97ron@users.noreply.github.com>
2021-04-18 09:45:31 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,8 @@
+#R_code_knitr
+#installiamo il pacchetto knitr
+install_packages("knitr")
+#settiamo la working directory ovvero quella in cui andiamo a pescare il nostro codice originale
+setwd("C:/lab/")
+
+# carichiamo il pacchetto knitr
+library(knitr)
