d7887c5af80d844ece1cc233aa6bbd9fc4d25ca8
nicole97ron <80778386+nicole97ron@users.noreply.github.com>
2022-12-27 11:08:40 +0100

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,44 +1,67 @@
-# R_code_classificazione.r 
+# R_code_classification.r 
 # CLASSIFICAZIONE: processo che accorpa pixel con valori simili, una volta che questi pixel sono stati accorpati rappresentano una classe
 
-# settiamo la working directory 
+# settiamo la working directory
 setwd("C:/lab/")
-# scarichiamo l'immagine che rappresenta i diversi livelli energetici di una parte all'interno del Sole
-# salviamo l'immmagine nella cartella lab
-# utilizziamo la funzione brick che permette di caricare il pacchetto di dati in R
+# impostiamo le librerie che ci servono
 library(raster)
-# questa è la libreria che ci serve per importare il dato
+library(RStoolbox) 
+
+# funzione brick: inseriamo l'immagine RGB con 3 livelli (pacchetto di dati) 
+# associamo l'oggetto so (solar orbiter) al risultato della funzione
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
-# con la freccia associamo la funzione ad un nome
-
+# vediamo le informazioni relative alle 3 bande
 so
-# con questa funzione otteniamo tutti i parametri
-# visualizziamo i tre livelli con la funzione plot
-plotRGB(so, 1, 2, 3, stretch="lin")
+# calss: RasterBrick 
+# values: 0 , 255 → immagine a 8 bit → 2^8 = 256 valori 
 
-# Unsuperviesd classification
-library(RStoolbox)
-# la funzione unsuperclass serve per procedere con la classificazione
+# visualizziamo i 3 livelli dell'immagine RGB
+# funzione plotRGB: ci serve per visualizzare i 3 livelli dell'immagine RGB
+# X = oggetto
+# banda 1 sulla componenete red, banda 2 sulla componente green, banda 3 sulla componente blue
+# stretch lineare
+plotRGB(so, 1, 2, 3, stretch="Lin")
+# vediamo diversi livelli energitici, da alti (colore acceso) a bassi (grigio/nero) 
+
+# classifichiamo l'immagine per individuare le 3 classi
+# classificazione non supervisionata: si lascia al software la possibilità di definire il training set sulla base delle riflettanze dei pixel
+# training set: il software cattura pochi pixel all’interno dell’immagine per creare un set di controllo e poi si classifica l'intera immagine
+# maximum likelihood: per ogni pixel si calcola la distanza nello spazio multispettrale, in base a questa si associa il pixel ad una determinata classe 
+# per la classificazione serve la library(RStoolbox) -> libreria che contiene la funzione unsuperClass
+# library(RStoolbox) 
+
+# funzione unsuperClass: opera la classificazione non supervisionata (dentro al pacchetto RStoolbox) 
+# so: inserimento dell'immagine 
+# nClasses=3: numero di classi sulla base dell'immagine 
+# associamo l'oggetto soc (solar orbiter classified) al risultato della funzione
 soc <- unsuperClass(so, nClasses=3)
-# inseriamo il nome dell'immagine e il numero di classi 
-# associamo la funzione ad un oggetto, ovvero "soc"
+
+# funzione unsuperClass: ha creato l'immaigine classificata (soc) formata da più pezzi: modello + mappa 
+# facciamo un plot dell'immagine classificata (soc) e in particolare della mappa
+# $: leghiamo l'immagine classificata (soc) alla sua mappa (map)
 plot(soc$map)
-# con questa funzione si ottengono classificazioni e quindi immagini differenti
-# per ottenere una classificazione univoca si usa la funzione "set.seed"
+# 3 classi: classe 1: bianca; classe 2: gialla; classe 3: verde
+# queste classi corrispondono ai diversi livelli di energia (alto - medio - basso)
+
+# la mia mappa finale è diversa dalle altre -> i pixel selezionati in entrata per il training set sono diversi di volta in volta
+# funzione set.seed: serve per fare una classificazione che sia sempre la stessa (usa sempre le stesse repliche per fare il modello) 
 set.seed(42)
 
-# Unsupervised Classification with 20 classes
-son <- unsuperClass(so, nClasses=20)
-plot(son$map)
+# esercitazione: Unsupervised Classification with 20 classes
+set.seed(42)
+soc20 <- unsuperClass(so, nClasses=20)
+plot(soc20$map)
 
-# Dowload an Image from
-# https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
-# scarichiamo l'immagine e salviamo nella cartella lab
-# rinominiamola come "sun"
-# facciamo la classificazione
+# esercitazione: download an image from: https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
+# funzione brick: importiamo l'immagine dentro R
 sun <- brick("sun.png")
+# classificazione dell'immaigne sun - Unsupervised classification
+# 3 classi 
+set.seed(42)
 sunc <- unsuperClass(sun, nClasses=3)
+# plottiamo la mappa dell'immagine sun classificata 
 plot(sunc$map)
+# -----------------------------------------------------------------------------------------------------------------------------
 
 # Grand Canyon 
 # richiamiamo le library
