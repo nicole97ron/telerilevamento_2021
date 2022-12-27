e526687db4af17fcb02834030ea1186a296cd737
nicole97ron <80778386+nicole97ron@users.noreply.github.com>
2022-12-27 10:56:28 +0100

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,14 +1,31 @@
-#R_code_knitr
-#installiamo il pacchetto knitr
-install_packages("knitr")
-#settiamo la working directory ovvero quella in cui andiamo a pescare il nostro codice originale
+#R_code_knitr.r
+
+# settiamo la working directory
 setwd("C:/lab/")
 
-# carichiamo il pacchetto knitr
+# richiamiamo tutte le library necessarie
+library (raster)
+library (rasterVis)
+# pacchetto knitr: può utilizzare un codice esterno (code.r) per creare un report
+install.packages("knitr") 
 library(knitr)
+# installare un altro pezzo di R per creare il report
+install.packages("tinytex")
+library(tinytex)
 
-#salviamo il codice da utilizzare nella cartella lab
-#il pacchetto knitr pesca il codice nella cartella lab e lo carica in R generando il report 
-#utilizziamo la funzione stitch
-stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+# prendiamo la repository sulla Groenlandia: la copiamo e la incolliamo sul blocco note
+# salviamo il blocco note (file di testo) nella cartella lab con il nome: R-code-Greenland.r
+# cambiamo l'estensione da .txt a .r
+# usiamo il pacchetto knitr che utilizza il codice appena salvato nella cartella lab, lo carica dentro a R e poi genera il report
 
+# funzione stitch: crea automaticamente un REPORT basato su uno script di R
+# primo argomento: nome del codice che abbiamo salvato nella cartella lab tra "" -> "R-code-Greenland.r"
+# "misc": il tipo di template all'interno di knitr
+# "knit-template.Rnw": il file di riferimento 
+# "knitr": il pacchetto che si utilizza 
+stitch("R-code-Greenland.r", template=system.file("misc","knitr-template.Rnw", package="knitr"))
+
+# 2 azioni per risolvere il problema latex 
+# Error: LaTex to compile R-code-Greenland.tex
+tinytex::install_tinytex()
+tinytex::tlmgr_update()
